af27ec7b6a7932663fb9abeedd790dbbae83b0d3
yanchen184 <62279165+yanchen184@users.noreply.github.com>
2025-05-08 10:50:56 +0800

    添加功能開關枚舉類

---
+++ src/main/java/com/yanchen/plugin/objecthelper/common/enums/FunctionSwitchEnum.java
@@ -0,0 +1,16 @@
+package com.yanchen.plugin.objecthelper.common.enums;
+
+/**
+ * 功能開關枚舉
+ */
+public enum FunctionSwitchEnum {
+    /**
+     * 開啟功能
+     */
+    OPEN,
+    
+    /**
+     * 關閉功能
+     */
+    CLOSE;
+}
