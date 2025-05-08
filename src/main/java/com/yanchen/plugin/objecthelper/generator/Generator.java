c7e20cf2b2468c03e2e85fcf2a8f45a1cbf8df8f
yanchen184 <62279165+yanchen184@users.noreply.github.com>
2025-05-08 11:09:19 +0800

    添加生成器接口

---
+++ src/main/java/com/yanchen/plugin/objecthelper/generator/Generator.java
@@ -0,0 +1,14 @@
+package com.yanchen.plugin.objecthelper.generator;
+
+/**
+ * 代碼生成器接口
+ */
+public interface Generator {
+    
+    /**
+     * 生成代碼
+     *
+     * @return 生成的代碼
+     */
+    String generate();
+}
