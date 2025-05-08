9b252376ccd3ce7cf696937b675ca86dbcf16a20
yanchen184 <62279165+yanchen184@users.noreply.github.com>
2025-05-08 10:49:12 +0800

    添加Java關鍵字常量類

---
+++ src/main/java/com/yanchen/plugin/objecthelper/common/constant/JavaKeyWord.java
@@ -0,0 +1,41 @@
+package com.yanchen.plugin.objecthelper.common.constant;
+
+/**
+ * Java 關鍵字常量
+ */
+public interface JavaKeyWord {
+    /**
+     * void 關鍵字
+     */
+    String VOID = "void";
+    
+    /**
+     * public 關鍵字
+     */
+    String PUBLIC = "public";
+    
+    /**
+     * private 關鍵字
+     */
+    String PRIVATE = "private";
+    
+    /**
+     * protected 關鍵字
+     */
+    String PROTECTED = "protected";
+    
+    /**
+     * static 關鍵字
+     */
+    String STATIC = "static";
+    
+    /**
+     * class 關鍵字
+     */
+    String CLASS = "class";
+    
+    /**
+     * interface 關鍵字
+     */
+    String INTERFACE = "interface";
+}
